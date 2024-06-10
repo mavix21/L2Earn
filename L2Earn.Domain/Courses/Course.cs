e0a76944f0e79539b58f1f7b9ea56cf55158afaf
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-06-10 00:18:45 -0500

    Init

---
+++ L2Earn.Domain/Courses/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace L2Earn.Domain.Courses;
+
+public sealed class Course
+{
+    public CourseId Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string ImageUrl { get; private set; }
+    public string Author { get; private set; }
+    public bool IsProgressLimited { get; private set; }
+}
---
+++ L2Earn.Domain/Courses/CourseId.cs
@@ -0,0 +1,3 @@
+﻿namespace L2Earn.Domain.Courses;
+
+public readonly record struct CourseId(Guid Id);
